4cc1092da0e82c0a517a8c4d50d3fb2e3da88c41
SafdarAliRajpura <hbrajpura110@gmail.com>
2026-03-31 20:43:06 +0530

    Updated

--- app.js
+++ app.js
@@ -40,7 +40,7 @@ connectDB().then(() => {
 
 
   const PORT = process.env.PORT || 5000;
-  app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+  app.listen(PORT,'0.0.0.0', () => console.log(`Server running on port ${PORT}`));
 }).catch((error) => {
   console.log('Failed to connect to database', error);
 });
